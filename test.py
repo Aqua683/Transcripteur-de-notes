b5ec53525f7e14abcc9b5731edab22fd2189c694
Aqua683 <edison21318@gmail.com>
2024-09-24 12:46:25 -0400

    Added Main Functionalities (Data Analysis)

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import pandas as pd
+from tabulate import tabulate
+from openpyxl import load_workbook
 
 # Read the Excel file
 df_RAW = pd.read_excel('MusicDatabase.xlsx', sheet_name = "RAW_DATA")
 df_PROCESSED = pd.read_excel('MusicDatabase.xlsx', sheet_name = "PROCESSED_DATA")
-
+df_REFERENCE = pd.read_excel('MusicDatabase.xlsx', sheet_name = "REFERENCE")
 
 def get_note_from_frequency(frequency):
     switcher = {
@@ -19,10 +21,36 @@ def get_note_from_frequency(frequency):
     # Return the corresponding note or a default message if not found
     return switcher.get(frequency, "Note: ")
 
+
+def get_rythmn_from_duration(duration, tempoFormula):
+    # Ratios for different note types based on real music rhythm
+    if duration == 4 * tempoFormula:
+        return "Whole Note"
+    elif duration == 2 * tempoFormula:
+        return "Half Note"
+    elif duration == tempoFormula:
+        return "Quarter Note"
+    elif duration == tempoFormula / 2:
+        return "Eighth Note"
+    elif duration == tempoFormula / 4:
+        return "Sixteenth Note"
+    else:
+        return "Unknown note type"
+
 for index, row in df_RAW.iterrows():
     freq = row["Frequency(Hz)"]
     note = get_note_from_frequency(freq)
-    print(f"{freq} Hz is: {note}")
+    duration = row["Duration(ms)"]
+    IdSong = row["IdSong"]
+    tempo = 60
+    tempoFormula = 60000/tempo
+    rythmn = get_rythmn_from_duration(duration, tempoFormula)
+    df_PROCESSED.loc[index, "Note"] = note
+    df_PROCESSED.loc[index, "Rythmn"] = rythmn
 
+
+print(tabulate(df_PROCESSED, headers='keys', tablefmt='pretty'))
+with pd.ExcelWriter('MusicDatabase.xlsx', mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
+    df_PROCESSED.to_excel(writer, sheet_name='PROCESSED_DATA', index=False)
 
 # Print the contents of the file
